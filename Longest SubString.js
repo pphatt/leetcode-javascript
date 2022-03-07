3b870dfc3a4e44f7cb6531ccb0b23c7fc5e71bca
phatshibukai <vutienphat0809@gmail.com>
2022-03-07 23:25:48 +0700

    Update

---
+++ Longest SubString.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function(s) {
+    let hashMap = {}
+    let num = []
+
+    for(let i = 0; i < s.length; i++) {
+        i = hashMap[s[i]]
+    }
+
+    for(let i = 0; i < s.length; i++) {
+        // if(s[i] )
+    }
+
+};
---
+++ TwoSum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+//
+// O(n^2) -> time complexity
+// 104ms -> time complexity
+// 42,7 MB -> space complexity
+//
+const twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        let rest = target - nums[i]
+        let complete = nums.indexOf(rest, i + 1)
+        if(complete !== -1) {
+            return [i, complete]
+        }
+    }
+    return [0, 0]
+};
+
+//
+// O(2n) -> time complexity
+// 85ms -> time complexity
+// 45 MB -> space complexity
+//
+solution = (nums, target) => {
+    let hashMap = {}
+
+    for(let i = 0; i < nums.length; i++) {
+        hashMap[nums[i]] = i
+    }
+
+    for(let i = 0; i < nums.length; i++) {
+        let rest = target - nums[i]
+        let found = hashMap[rest]
+        if(found !== undefined && found !== i) {
+            return [i, found]
+        }
+    }
+    return [0,0];
+}
+
+console.log(twoSum([3,2,4], 6))
+console.log(solution([3,2,3], 6))
